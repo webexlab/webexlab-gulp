669720954d052d52f3a5d139b69950fcffc41ee1
unknown <i.uraksin@gmail.com>
2020-11-01 11:15:44 +0400

    Version 1.0

---
+++ src/js/script.js
@@ -0,0 +1,23 @@
+@@include("jquery.js");
+@@include("bootstrap.js");
+
+// Проверка поддержки браузером изображений в формате .webp
+
+function testWebP(callback) {
+    var webP = new Image();
+    webP.onload = webP.onerror = function () {
+        callback(webP.height == 2);
+    };
+    webP.src =
+        "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+}
+
+// Добавления класса webp к тегу body для использования webp в css файлах
+
+testWebP(function (support) {
+    if (support == true) {
+        document.querySelector("body").classList.add("webp");
+    } else {
+        document.querySelector("body").classList.add("no-webp");
+    }
+});
